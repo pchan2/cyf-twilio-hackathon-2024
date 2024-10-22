d80883a9790a57f6028688d4bf92628b36e8a63f
Patrick Chan <q_p18@hotmail.com>
2024-10-23 00:42:14 +0100

    Reword 'mode' to 'theme'.

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@ const theme = localStorage.getItem("theme") || "";
 const themeToggle = document.querySelector(".themeToggle");
 const body = document.querySelector("body");
 
-document.body.className = mode;
+document.body.className = theme;
 
 themeToggle.addEventListener("click", () => {  
     localStorage.setItem("theme", theme === "light" ? "" : "light")
