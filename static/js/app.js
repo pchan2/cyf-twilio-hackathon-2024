83ebf6c0f78a150248e9fce3e74e9b46146b3576
Patrick Chan <q_p18@hotmail.com>
2024-10-23 01:06:33 +0100

    Toggle login status to display elements accordingly.

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,24 @@
 const theme = localStorage.getItem("theme") || "";
 const themeToggle = document.querySelector(".themeToggle");
+const loginStatus = localStorage.getItem("loginStatus") || "";
+const logInButton = document.querySelector(".logInButton");
+const logOutButton = document.querySelector(".logOutButton");
 const body = document.querySelector("body");
 
 document.body.className = theme;
+document.body.className = loginStatus;
 
 themeToggle.addEventListener("click", () => {  
     localStorage.setItem("theme", theme === "light" ? "" : "light")
     body.classList.toggle("light")
+})
+
+logInButton.addEventListener("click", () => {  
+    localStorage.setItem("loginStatus", loginStatus === "loggedIn" ? "" : "loggedIn")
+    body.classList.toggle("loggedIn")
+})
+
+logOutButton.addEventListener("click", () => {  
+    localStorage.setItem("loginStatus", loginStatus === "loggedIn" ? "" : "loggedIn")
+    body.classList.toggle("loggedIn")
 })
